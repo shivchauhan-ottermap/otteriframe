2418c51be2743a55c193622d7eac96e295df1e80
Shiv Chauhan <shiv.chauhan@ottermap.com>
2026-03-20 17:02:18 +0530

    fixing package.json

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
 
   return (
-    <div className="bg-slate-300 h-screen w-screen flex justify-center pt-5">
+    <div style={{ backgroundColor: "slategray", width: '100%', height: '100%', display: "flex", justifyItems: "center", alignItems: "center", paddingTop: '5px' }}>
       <iframe src="https://dev.ottermap.com/terraorder?auth=9d62b353b17045f2aa6fc5c77a6b87876404621d" width={'80%'} height={'95%'}></iframe>
     </div>
   )
