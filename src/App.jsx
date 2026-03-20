6c4352d4e5f59f265d35de7ab76a2ce5edad8fd1
Shiv Chauhan <shiv.chauhan@ottermap.com>
2026-03-20 17:10:29 +0530

    ui fixes done

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,24 @@
 function App() {
 
   return (
-    <div style={{ backgroundColor: "slategray", width: '100%', height: '100%', display: "flex", justifyItems: "center", alignItems: "center", paddingTop: '5px' }}>
-      <iframe src="https://dev.ottermap.com/terraorder?auth=9d62b353b17045f2aa6fc5c77a6b87876404621d" width={'80%'} height={'95%'}></iframe>
+    <div
+      style={{
+        width: "100%",
+        height: "95vh",
+        display: "flex",
+        justifyItems: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <iframe
+        src="https://dev.ottermap.com/terraorder?auth=9d62b353b17045f2aa6fc5c77a6b87876404621d"
+        width="80%"
+        height="90%"
+        style={{ border: "none" }}
+        title="OtterMap TerraOrder"
+      />
     </div>
   )
 }
