68592f9fb444f3a26673cbc6c42ac5776824edff
Shiv Chauhan <shiv.chauhan@ottermap.com>
2026-03-20 16:25:24 +0530

    added otter iframe project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="bg-slate-300 h-screen w-screen flex justify-center pt-5">
+      <iframe src="https://dev.ottermap.com/terraorder?auth=9d62b353b17045f2aa6fc5c77a6b87876404621d" width={'80%'} height={'95%'}></iframe>
+    </div>
+  )
+}
+
+export default App
